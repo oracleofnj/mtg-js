4aaff5d9ddccd20322b951547ce1c775130b8f59
oracleofnj <jared.samet@aya.yale.edu>
2015-12-06 23:41:25 -0500

    Add amortization schedule calcs

--- mtg-calcs.js
+++ mtg-calcs.js
@@ -1,11 +1,48 @@
 var finance = (function(){
     pmt = function(rate, nper, pv) {
-        accrualFactor = 1.0 + rate;
-        finalFV = Math.pow(accrualFactor, nper);
+        var finalFV = Math.pow(1.0 + rate, nper);
         return (rate * pv * finalFV) / (finalFV - 1.0);
     }
 
+    nextPeriod = function(prevPeriod, extraPrincipal) {
+        var period, interest, principal;
+        period = prevPeriod.period + 1;
+        if (prevPeriod.endingBalance > 0.005) {
+            interest = prevPeriod.endingBalance * prevPeriod.interestRate / 12.0;
+            principal = Math.min(prevPeriod.endingBalance, prevPeriod.monthlyPayment - interest);
+        } else {
+            interest = 0;
+            principal = 0;
+        }
+        return {
+            baseDate: prevPeriod.baseDate,
+            monthlyPayment: prevPeriod.monthlyPayment,
+            period: period,
+            paymentDate: prevPeriod.baseDate.clone().add(moment.duration(period, 'months')),
+            interestRate: prevPeriod.interestRate,
+            interest: interest,
+            principal: principal,
+            requiredPayment: interest + principal,
+            totalPayment: interest + principal + extraPrincipal,
+            endingBalance: prevPeriod.endingBalance - principal - extraPrincipal
+        };
+    }
+
+    amortizationSchedule = function(annualRate, nper, pv, firstMoment, extraPayments) {
+        startPeriod = {
+            baseDate: firstMoment,
+            monthlyPayment: pmt(annualRate/12.0, nper, pv),
+            period: 0,
+            endingBalance: pv,
+            interestRate: annualRate
+        };
+        return extraPayments.map(function(extraPrincipal) {
+            return (startPeriod = nextPeriod(startPeriod, extraPrincipal))
+        });
+    }
+
     return {
-        pmt : pmt
+        pmt : pmt,
+        amortizationSchedule : amortizationSchedule
     };
 })();
--- mtg-widget.js
+++ mtg-widget.js
@@ -1,16 +1,33 @@
 function updatePayment() {
-    var payment = finance.pmt(
-        $('#interest_rate').val() / 1200.0,
-        $('#loan_term').val() * 12,
-        parseFloat($('#loan_amount').val())
-    );
+    var nPeriods = $('#loan-term').val() * 12;
+    var interestRate = $('#interest-rate').val() / 100.0;
+    var loanAmount = parseFloat($('#loan-amount').val());
+    var payment = finance.pmt(interestRate / 12, nPeriods, loanAmount);
+    var baseMoment = moment($('#closing-date').val(), 'DD-MMM-YYYY');
+    var extraPayments = [];
+    for (var i=0; i < nPeriods; i++) {
+        extraPayments[i] = 0;
+    }
 
-    $('#monthly_payment').html('$' + payment.toFixed(2));
+    amSched = finance.amortizationSchedule(interestRate, nPeriods, loanAmount, baseMoment, extraPayments);
+    var s = '';
+    $('#monthly-payment').html('$' + payment.toFixed(2));
+    for (i = 0; i < nPeriods; i++) {
+        s = s + '<tr><td>' +
+                amSched[i].period + '</td><td>' +
+                amSched[i].paymentDate.format('DD-MMM-YYYY') + '</td><td>' +
+                (amSched[i].interestRate * 100).toFixed(3) + '</td><td>' +
+                '$' + amSched[i].interest.toFixed(2) + '</td><td>' +
+                '$' + amSched[i].principal.toFixed(2) + '</td><td>' +
+                '$' + amSched[i].requiredPayment.toFixed(2) + '</td><td>' +
+                '$' + extraPayments[i].toFixed(2) + '</td><td>' +
+                '$' + amSched[i].totalPayment.toFixed(2) + '</td><td>' +
+                '$' + amSched[i].endingBalance.toFixed(2) + '</td></tr>';
+    }
+    $('#amortization-schedule').html(s);
 }
 
 $(document).ready(function() {
-    $('#loan_amount').change(updatePayment);
-    $('#interest_rate').change(updatePayment);
-    $('#loan_term').change(updatePayment);
+    $('#loan-option-form').find("input").change(updatePayment);
     updatePayment();
 });
